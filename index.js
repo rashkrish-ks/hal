14c743a6b714cff4793dbe9c52bcb5072dfd06cc
Rashmi k s <98524109+rashkrish-ks@users.noreply.github.com>
2023-07-07 16:14:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const app = express();
+var path=require('path');
+const port = 3000
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true })); 
+ app.use(bodyParser.json()); 
+
+ app.use(express.static('resources'));
+ app.get("/", (req, res) => {
+     res.sendFile(path.join(__dirname, 'resources/etr.html'))
+    });
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
